b266ad1a491286d6e8f3dc73b8f070625a74043b
LartIgnacio <ace.ignacio@gmail.com>
2015-05-17 21:41:37 +0800

    Initial Implementation for sample today widgets.

---
+++ UsedSpace Widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  UsedSpace Widget
+//
+//  Created by Lart Ace Ignacio on 5/17/15.
+//  Copyright (c) 2015 Lart Ace Ignacio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ UsedSpace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UsedSpace
+//
+//  Created by Lart Ace Ignacio on 5/17/15.
+//  Copyright (c) 2015 Lart Ace Ignacio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
